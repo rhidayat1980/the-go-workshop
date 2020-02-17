2d6a68b9a0105fb7e5aa47b23aa285970dd06ab9
rachmat-hidayat <rachmat.hidayat@rea-group.com>
2020-02-17 14:12:17 +0800

    add looping over string

--- 03-core-types/exercise-3.05/main.go
+++ 03-core-types/exercise-3.05/main.go
@@ -10,16 +10,22 @@ func main() {
 		fmt.Println(index, string(rune(runeVal)))
 	}
 
+	fmt.Println("")
 	name := "John Doe"
 
 	for k, v := range name {
 		fmt.Println(k, v)
 	}
-
+	fmt.Println("")
 	for k, v := range string(name) {
 		fmt.Println(k, v)
 	}
 
+	fmt.Println("")
+	for key, value := range name {
+		fmt.Println(key, string(rune(value)))
+	}
+	fmt.Println("")
 	for k, v := range name {
 		fmt.Println(k, string(rune(v)))
 	}

8a9b4a31055f5f0b4fe313872b1573300a4ba789
rachmat-hidayat <rachmat.hidayat@rea-group.com>
2020-02-17 14:54:30 +0800

    add nil value

---
+++ 03-core-types/nil-value/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message *string
+
+	if message == nil {
+		fmt.Println("error, unexpected nil value")
+		return
+	}
+
+	fmt.Println(&message)
+	fmt.Println(message)
+}
